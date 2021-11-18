acb62dcc86f8f8f7917eeb55fc584562e634dfc3
David Cruciani <da.cruciani@laposte.net>
2021-11-18 17:00:32 +0100

    chg: [feeder] add api use + sleep

--- bin/traitement_gharchive.py
+++ bin/traitement_gharchive.py
@@ -1,20 +1,28 @@
 import os
+import re
 import json
 import gzip
+import time
 import hashlib
 import pathlib
 import argparse
 import datetime
 import requests
+import subprocess
 import configparser
 from uuid import uuid4
 
 pathProg = pathlib.Path(__file__).parent.absolute()
 
-uuid = "80172ead-7023-496c-a4be-6ee280d8fbcf"
-
 config = configparser.ConfigParser()
 config.read('../etc/config.cfg')
+
+if 'general' in config:
+    uuid = config['general']['uuid']
+
+if 'github' in config:
+    api_token = config['github']['api_token']
+
 
 def json_patch(element, i, j, cpPatch):
     json_patch = {}
@@ -46,7 +54,7 @@ def json_patch(element, i, j, cpPatch):
         meta_dict["github:org_name"] = ""
 
     meta_dict["github:datestamp"] = date
-    meta_dict["github:timestamp"] = time
+    meta_dict["github:timestamp"] = time_element
     meta_dict["github:timezone"] = "UTC"
 
     meta_dict["github:parent"] = uuid_parent
@@ -98,7 +106,7 @@ def json_commit(element, i, cpCommit, flagCommitDelete, flagRepoDelete):
         meta_commit["github:org_name"] = ""
 
     meta_commit["github:datestamp"] = date
-    meta_commit["github:timestamp"] = time
+    meta_commit["github:timestamp"] = time_element
     meta_commit["github:timezone"] = "UTC"
 
     if flagRepoDelete:
@@ -122,15 +130,25 @@ def json_commit(element, i, cpCommit, flagCommitDelete, flagRepoDelete):
     
     return cpCommit
 
+def subprocessCall(request):
+    p = subprocess.Popen(request, stdout=subprocess.PIPE, shell=True)
+    (output, err) = p.communicate()
+    p_status = p.wait()
 
+    return output
 
 
 def traitement_json(element, i, cpPatch, cpCommit):
     flagRepoDelete = False
     flagCommitDelete = False
 
-    response = requests.get(element["payload"]["commits"][i]["url"])
-    json_api = json.loads(response.text)
+    flagRecur = False
+
+    header = {'Authorization': 'token ' + api_token}
+    response = requests.get(element["payload"]["commits"][i]["url"], headers=header)
+    # response = requests.get(element["payload"]["commits"][i]["url"])
+
+    json_api = json.loads(response.content)
 
     if "message" in json_api:
         ## The Repository has been deleted
@@ -140,117 +158,191 @@ def traitement_json(element, i, cpPatch, cpCommit):
         if "No commit found" in json_api["message"]:
             flagCommitDelete = True
         if "API rate limit exceeded" in json_api["message"]:
-            print("\n\n[-] API rate limit exceeded")
-            exit(-1)
+
+            time_remain = datetime.datetime.fromtimestamp(int(response.headers['X-RateLimit-Reset'])).strftime('%Y-%m-%d %H:%M:%S')
+            time_remain = datetime.datetime.strptime(time_remain, "%Y-%m-%d %H:%M:%S")
+            diff = abs(time_remain - datetime.datetime.now())
+
+            print("\n\n[-] API rate limit exceeded, sleep for %s" % (diff))
+            time.sleep(diff.total_seconds() + 10)
+
+            flagRecur = True
+            cpPatch, cpCommit = traitement_json(element,i,cpPatch,cpCommit)
     else:
         for j in json_api["files"]:
             if "patch" in j:
                 cpPatch = json_patch(element, i, j, cpPatch)
-    
-    cpCommit = json_commit(element, i, cpCommit, flagCommitDelete, flagRepoDelete)
+    if not flagRecur:
+        cpCommit = json_commit(element, i, cpCommit, flagCommitDelete, flagRepoDelete)
 
     return cpPatch, cpCommit
+
+
+
 
 
 # arguments parsing
 
 parser = argparse.ArgumentParser()
-parser.add_argument("datetime", help="date of the GHArchive, YYYY-MM-DD-H, YYYY-MM-DD-{H..H}, YYYY-MM-{DD..DD}-{H..H}")
+parser.add_argument("datetime", help="date of the GHArchive, YYYY-MM-DD-H, YYYY-MM-DD-{H..H}, YYYY-MM-{DD..DD}-{H..H}, YYYY-MM-{DD..DD}-H")
 parser.add_argument("--nocache", help="disable cache", action="store_true")
-parser.add_argument("-u", "--users", help="search username", action="store_true")
-parser.add_argument("-o", "--org", help="search organisation", action="store_true")
+parser.add_argument("-u", "--users", help="search username")
+parser.add_argument("-o", "--org", help="search organisation")
 parser.add_argument("-l", "--list", help="list of word to search. If no list is give, all commit message will be add")
 args = parser.parse_args()
 
-url = "https://data.gharchive.org/%s.json.gz" % (args.datetime)
-filename = url.split("/")[-1]
-pathTemp = os.path.join(pathProg, filename)
+
+x = re.match(r"[0-9]{4}\-[0-9]{2}\-\{?[0-9]{1,2}\.{0,2}[0-9]{0,2}\}?\-\{?[0-9]{1,2}\.{0,2}[0-9]{0,2}\}?", args.datetime)
+if x == None:
+    print("[-] Date Format Error")
+    exit(-1)
+
+
+pathArchive = os.path.join(pathProg, "archive")
 pathPatch = os.path.join(pathProg, "patch")
 pathCommit = os.path.join(pathProg, "commit")
 pathError = os.path.join(pathProg, "error.json")
 
+if not os.path.isdir(pathArchive):
+    os.mkdir(pathArchive)
+
 if args.list:
     with open(args.list, "r") as read_file:
         list_leak = read_file.readlines()
+if args.org:
+    with open(args.org, "r") as read_file:
+        list_org = read_file.readlines()
+if args.users:
+    with open(args.users, "r") as read_file:
+        list_users = read_file.readlines()
 
 ## Download archive file
 print("[+] Downloading...")
-r = requests.get(url)
+if "{" in args.datetime:
+    range_list = list()
+    currentDate = args.datetime.split("{")
 
-if len(r.content) == 127:
-    print("[-] Date Format Error")
-    exit(-1)
-    
-if not os.path.isfile(pathTemp):
-    with open(pathTemp, "wb") as f:
-                   
-        f.write(r.content)
+    for element in currentDate:
+        if "}" in element:
+            range_list.append(re.findall(r"[0-9]+", element))
 
-## Open json file
-print("[+] Unizp...")
-data = [json.loads(line) for line in gzip.open(pathTemp, 'r')]
-# with open(pathTemp, "r") as json_read:
-    # data = json.load(json_read)
+    ## YYYY-MM-{DD..DD}-H
+    if re.search(r"{.*}$", str(args.datetime)) == None:
+        if int(range_list[0][0]) > 0 and int(range_list[0][1]) < 32:
+            for i in range(int(range_list[0][0]), int(range_list[0][1]) + 1):
+                if i < 10:
+                    url = "https://data.gharchive.org/%s0%s-%s.json.gz" % (currentDate[0], i, range_list[0][2])
+                else:
+                    url = "https://data.gharchive.org/%s%s-%s.json.gz" % (currentDate[0], i, range_list[0][2])
 
-
-print("[+] Traitment...")
-ele_list = list()
-
-for element in data:
-    if element["type"] == "PushEvent":
-        flag = False
-        if args.org or args.users:
-            if "org" in element and args.org == element["org"]["login"]:
-                # ele_list.append(element)
-                # continue
-                flag = True
-            for i in range(0, len(element["payload"]["commits"])):
-                if args.users == element["payload"]["commits"][i]["author"]["name"]:
-                    flag = True
-                    break
-
-        ## org or user match with entry
-        if flag or (not args.org and not args.users):
-            """if args.list:
-                flagList = False
-                for i in range(0, len(element["payload"]["commits"])):
-                    for lines in list_leak:
-                        if lines.rstrip("\n") in element["payload"]["commits"][i]["message"]:
-                            ele_list.append(element)
-                            flagList = True
-                            break
-                    if flagList:
-                        break
-            ## There's no list-leak, so element is add without check commit message
-            else:"""
-            ele_list.append(element)
-
-print("[+] Rule Creation")
-
-if not os.path.isdir(pathCommit):
-    os.mkdir(pathCommit)
-if not os.path.isdir(pathPatch):
-    os.mkdir(pathPatch)
-
-## Rule creation
-cpCommit = 0
-cpPatch = 0
-for element in ele_list:
-    date = datetime.datetime.strptime(element["created_at"], "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d")
-    time = datetime.datetime.strptime(element["created_at"], "%Y-%m-%dT%H:%M:%SZ").strftime("%H:%M:%S")
-
-    uuid_parent = str(uuid4())
-
-    for i in range(0,len(element["payload"]["commits"])):
-        if args.list:
-            for lines in list_leak:
-                if lines.rstrip("\n") in element["payload"]["commits"][i]["message"]:
-                    cpPatch, cpCommit = traitement_json(element, i, cpPatch, cpCommit)
+                request = "wget %s -P %s" % (url, pathArchive)
+                subprocessCall(request)
         else:
-            cpPatch, cpCommit = traitement_json(element, i, cpPatch, cpCommit)
+            print("[-] Date Value Error for Days")
+            exit(-1)
+    
+    ## YYYY-MM-DD-{HH..HH}
+    if len(range_list) == 1:
+        if int(range_list[0][0]) >= 0 and int(range_list[0][1]) < 24:
+            for i in range(int(range_list[0][0]), int(range_list[0][1]) + 1):
+                url = "https://data.gharchive.org/%s%s.json.gz" % (currentDate[0], i)
+
+                request = "wget %s -P %s" % (url, pathArchive)
+                subprocessCall(request)
+        else:
+            print("[-] Date Value Error for Hours")
+            exit(-1)
+
+    ## YYYY-MM-{DD..DD}-{H..H}
+    elif len(range_list) == 2:
+        if ( int(range_list[0][0]) > 0 and int(range_list[0][1]) < 32 ) and int(range_list[0][0]) >= 0 and int(range_list[0][1]) < 24:
+            for i in range(int(range_list[0][0]), int(range_list[0][1]) + 1):
+                for j in range(int(range_list[0][0]), int(range_list[0][1]) + 1):
+                    if i < 10:
+                        url = "https://data.gharchive.org/%s0%s-" % (currentDate[0], i)
+                    else:
+                        url = "https://data.gharchive.org/%s%s-" % (currentDate[0], i)
+                    url += "%s.json.gz" % (j)
+
+                    request = "wget %s -P %s" % (url, pathArchive)
+                    subprocessCall(request)
+        else:
+            print("[-] Date Value Error for Days or Hours")
+else:
+    loc = args.datetime.split("-")
+    if (int(loc[1]) > 0 and int(loc[1]) < 13) and (int(loc[2]) > 0 and int(loc[2]) < 32) and (int(loc[3]) >= 0 and int(loc[3]) < 24):
+        url = "https://data.gharchive.org/%s.json.gz" % (args.datetime)
+        request = "wget %s -P %s" % (url, pathArchive)
+        subprocessCall(request)
 
 
-                    
-    print("\r[+] Commit JSON files: %s, Patch JSON files: %s" % (cpCommit, cpPatch), end="")
-    # print("\r[+] Patch JSON files: %s" % (cpCommit), end="")
+for archive in os.listdir(pathArchive):
+    currentArchive = os.path.join(pathArchive, archive)
 
+    ## Open json file
+    print("[+] Unizp...")
+    data = [json.loads(line) for line in gzip.open(currentArchive, 'r')]
+
+    print("[+] Traitment...")
+    ele_list = list()
+
+    for element in data:
+        if element["type"] == "PushEvent":
+            flag = False
+            if args.org:
+                if "org" in element:
+                    for orgs in list_org:
+                        if orgs.rstrip("\n") == element["org"]["login"]:
+                            flag = True
+                            break
+            if args.users:
+                for i in range(0, len(element["payload"]["commits"])):
+                    for users in list_users:
+                        if users.rstrip("\n") == element["payload"]["commits"][i]["author"]["name"]:
+                            flag = True
+                            break
+                    if flag:
+                        break
+
+            ## org or user match with entry
+            if flag or (not args.org and not args.users):
+                """if args.list:
+                    flagList = False
+                    for i in range(0, len(element["payload"]["commits"])):
+                        for lines in list_leak:
+                            if lines.rstrip("\n") in element["payload"]["commits"][i]["message"]:
+                                ele_list.append(element)
+                                flagList = True
+                                break
+                        if flagList:
+                            break
+                ## There's no list-leak, so element is add without check commit message
+                else:"""
+                ele_list.append(element)
+
+    print("[+] Rule Creation")
+
+    if not os.path.isdir(pathCommit):
+        os.mkdir(pathCommit)
+    if not os.path.isdir(pathPatch):
+        os.mkdir(pathPatch)
+
+    ## Rule creation
+    cpCommit = 0
+    cpPatch = 0
+    for element in ele_list:
+        date = datetime.datetime.strptime(element["created_at"], "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d")
+        time_element = datetime.datetime.strptime(element["created_at"], "%Y-%m-%dT%H:%M:%SZ").strftime("%H:%M:%S")
+
+        uuid_parent = str(uuid4())
+
+        for i in range(0,len(element["payload"]["commits"])):
+            if args.list:
+                for lines in list_leak:
+                    if lines.rstrip("\n") in element["payload"]["commits"][i]["message"]:
+                        cpPatch, cpCommit = traitement_json(element, i, cpPatch, cpCommit)
+            else:
+                cpPatch, cpCommit = traitement_json(element, i, cpPatch, cpCommit)
+               
+        print("\r[+] Commit JSON files: %s, Patch JSON files: %s" % (cpCommit, cpPatch), end="")
+        
